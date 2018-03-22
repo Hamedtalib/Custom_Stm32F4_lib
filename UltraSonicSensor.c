08fa8608b6cd0b221f98596458f7caf4117f4adf
Tyler Thompson <tyler.l.thompson@wmich.edu>
2018-03-22 15:51:50 -0400

    ultra sonic sensor working...

--- UltraSonicSensor.c
+++ UltraSonicSensor.c
@@ -1,4 +1,4 @@
-/**
+/** 
  * Driver for Ultra Sonic Sensor
  * Model: HC-SR4
  *
@@ -12,40 +12,82 @@
 
    __IO uint32_t echo_count = 0;
    float echo_delay = 0;
-   float distance_cm = 0;
-/**
-  * @brief  This function handles Trigger TIM global interrupt request.
-  * @param  None
-  * @retval None
-  */
-void TIM3_IRQHandler(void)
-{ 
-  if(TIM_GetITStatus(TRIG_TIM, TIM_IT_Update) != RESET) 
-  {
-    TIM_ClearITPendingBit(TRIG_TIM, TIM_IT_Update);
-  
-    
-    
-    
-    
-  }
-}
+  __IO uint32_t distance_cm = 0;
+  __IO uint32_t distance_in = 0;
+  __IO uint32_t echo_time = 0;
+   __IO uint32_t uhIC3ReadValue1 = 0;
+__IO uint32_t uhIC3ReadValue2 = 0;
+__IO uint32_t uhIC3ReadValue3 = 0;
+__IO uint32_t uhCaptureNumber = 0;
+__IO uint32_t uwCapture1 = 0;
+__IO uint32_t uwCapture2 = 0;
+__IO uint32_t uwTIM2Freq = 0;
+__IO uint32_t uwTIM2Duty = 0;
+
+__IO uint8_t duty = 0;
+__IO uint32_t capture_trig = 0;
+uint32_t pulse_count = 0;
+//__IO uint32_t echo_count = 0;
+
+
 
 /**
   * @brief  This function handles Echo TIM global interrupt request.
   * @param  None
   * @retval None
   */
-void TIM2_IRQHandler(void)
+void TIM4_IRQHandler(void)
 { 
-  if(TIM_GetITStatus(ECHO_TIM, TIM_IT_CC3) != RESET) 
+  if(TIM_GetITStatus(ECHO_TIM, TIM_IT_CC1) == SET) 
   {
-    TIM_ClearITPendingBit(ECHO_TIM, TIM_IT_CC3);
-   
-    echo_count++;
+    /* Clear TIM2 Capture compare interrupt pending bit */
+    TIM_ClearITPendingBit(ECHO_TIM, TIM_IT_CC1);
+    if(uhCaptureNumber == 0)
+    {
+      /* Get the Input Capture value */
+      uhIC3ReadValue1 = TIM_GetCapture1(ECHO_TIM);
+      uhCaptureNumber = 1;
+    }
+    else if(uhCaptureNumber == 1)
+    {
+      /* Get the Input Capture value */
+      uhIC3ReadValue2 = TIM_GetCapture1(ECHO_TIM); 
+      
+      /* Capture computation */
+     if (uhIC3ReadValue2 > uhIC3ReadValue1)
+     {
+        uwCapture1 = (uhIC3ReadValue2 - uhIC3ReadValue1);
+     }
+     else if (uhIC3ReadValue2 < uhIC3ReadValue1)
+     {
+        uwCapture1 = ((0xFFFF - uhIC3ReadValue1) + uhIC3ReadValue2);
+     }
+     else
+     {
+        uwCapture1 = 0;
+     }
+     
+     echo_time = ((uwCapture1 / (SystemCoreClock / 2000000 )) );
+     distance_cm = (echo_time / 58);
+     distance_in = (echo_time / 148);
+     uhCaptureNumber = 0;
+      
+    }
+    
+     
+     
+      
+    
   }
 }
-     
+
+uint32_t get_distance_cm(void) {
+  return distance_cm;
+}
+
+uint32_t get_distance_in(void) {
+  return distance_in;
+}
 
 /**
   * @brief  Initialize the Ultra Sonic sensor
@@ -53,9 +95,10 @@ void TIM2_IRQHandler(void)
   * @retval None
   */
 void ultrasonic_init(void) {
-  //_input_capture_config();
-  //_output_compare_config();
-  delay_init();
+  
+  _output_compare_config();
+  _input_capture_config();
+  //delay_init();
 }
 
 /**
@@ -64,33 +107,32 @@ void ultrasonic_init(void) {
   * @retval None
   */
 void ultrasonic_trig(void) {
-  echo_count = 0;
-  echo_delay = 0;
+
   _output_compare_config();
-  while (1) {
-    if(TIM3->CNT >= 2 || echo_delay > 36000){
-      RCC_APB1PeriphClockCmd(TRIG_TIM_CLK, DISABLE);
-      break;
-    }
-  }
+  //while (1) {
+  //  if(TIM3->CNT >= 2 || echo_delay > 36000){
+  //    RCC_APB1PeriphClockCmd(TRIG_TIM_CLK, DISABLE);
+  //    break;
+  //  }
+  //}
 }
 
 void ultrasonic_listen(void) {
   
   _input_capture_config();
   
-  
+  /*
   while(1) {
     echo_delay ++;
     if(echo_count == 2) {
       break;
     }
     delay_micro(1);
-  }
+  }*/
   //RCC_APB1PeriphClockCmd(ECHO_TIM_CLK, DISABLE);
   /* calc distance */
-  distance_cm = ((echo_delay - 550) / 58 ) ;
-  delay(1000);
+  //distance_cm = ((echo_delay - 550) / 58 ) ;
+  //delay(1000);
 }
 
 /**
@@ -137,10 +179,10 @@ static void _input_capture_config(void) {
   TIM_ICInit(ECHO_TIM, &TIM_ICInitStructure);
 
   /* TIM enable counter */
-  //TIM_Cmd(ECHO_TIM, ENABLE);
+  TIM_Cmd(ECHO_TIM, ENABLE);
 
   /* Enable the CC3 Interrupt Request */
-  TIM_ITConfig(ECHO_TIM, TIM_IT_CC3, ENABLE);
+  TIM_ITConfig(ECHO_TIM, TIM_IT_CC1, ENABLE);
 
 }
 
@@ -155,7 +197,6 @@ static void _output_compare_config(void) {
   GPIO_InitTypeDef GPIO_InitStructure;
   TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
   TIM_OCInitTypeDef  TIM_OCInitStructure;
-  NVIC_InitTypeDef NVIC_InitStructure;
 
   /* GPIO clock enable */
   RCC_AHB1PeriphClockCmd(TRIG_GPIO_CLK, ENABLE);
@@ -175,25 +216,24 @@ static void _output_compare_config(void) {
   RCC_APB1PeriphClockCmd(TRIG_TIM_CLK, ENABLE);
 
   /* Compute the value to be set in ARR regiter to generate signal frequency */
-  uint16_t TimerPeriod = (SystemCoreClock / (50000 * 2) ) - 1;
-  //50% duty cycle
-  uint16_t CCR1_Val = (uint16_t) (((uint32_t) 5 * (TimerPeriod - 1)) / 10);
+  //uint16_t TimerPeriod = (SystemCoreClock / (20 * 2) ) - 1;
+  //uint16_t TimerPeriod = 65535;
+  //uint32_t TimerPeriod = 0xFFFFFFFF;
+  uint32_t TimerPeriod = 4200000;
+  //duty cycle
+  uint16_t CCR1_Val = (uint32_t) (((float) 0.0016 * (TimerPeriod - 1)) / 10);
+  //uint16_t CCR1_Val = (uint16_t) (((uint32_t) 5 * (TimerPeriod - 1)) / 10);
+  //uint16_t CCR1_Val = 840;
+  //uint32_t CCR1_Val = 1176;
 
   /* Time base configuration */
   TIM_TimeBaseStructure.TIM_Period = TimerPeriod;
   TIM_TimeBaseStructure.TIM_Prescaler = 0;
   TIM_TimeBaseStructure.TIM_ClockDivision = 0;
-  TIM_TimeBaseStructure.TIM_RepetitionCounter = 0;
   TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
 
   TIM_TimeBaseInit(TRIG_TIM, &TIM_TimeBaseStructure);
   
-  /* Enable the TIM global Interrupt */
-  NVIC_InitStructure.NVIC_IRQChannel = TRIG_TIM_CHANNEL	;
-  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
-  NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
-  NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-  NVIC_Init(&NVIC_InitStructure);
 
   /* PWM1 Mode configuration: Channel1 */
   TIM_OCInitStructure.TIM_OCMode = TRIG_PWM_CHANNEL;
@@ -205,13 +245,6 @@ static void _output_compare_config(void) {
   
    /* TIM enable counter */
   TIM_Cmd(TRIG_TIM, ENABLE);
-  
-  /* Enable the CC3 Interrupt Request */
-  TIM_ITConfig(ECHO_TIM, TIM_IT_CC3, ENABLE);
-  
-  /* Single pulse mode */
-  //TIM_SelectOnePulseMode(TRIG_TIM, TIM_OPMode_Single);
-  
-  
+    
 
 }
--- UltraSonicSensor.h
+++ UltraSonicSensor.h
@@ -11,25 +11,26 @@
 
 #define __ULTRASONICSENSOR_H
 
-#define ECHO_GPIO_PORT	GPIOA
-#define ECHO_GPIO_PIN	GPIO_Pin_2
-#define ECHO_GPIO_CLK	RCC_AHB1Periph_GPIOA
-#define ECHO_TIM	TIM2
-#define ECHO_TIM_CLK	RCC_APB1Periph_TIM2
-#define ECHO_TIM_PIN	GPIO_PinSource2
-#define ECHO_TIM_AF	GPIO_AF_TIM2
-#define ECHO_TIM_CHANNEL	TIM_Channel_3	
-#define ECHO_TIM_IRQ	TIM2_IRQn;
+#define ECHO_GPIO_PORT	GPIOB
+#define ECHO_GPIO_PIN	GPIO_Pin_6
+#define ECHO_GPIO_CLK	RCC_AHB1Periph_GPIOB
+#define ECHO_TIM	TIM4
+#define ECHO_TIM_CLK	RCC_APB1Periph_TIM4
+#define ECHO_TIM_PIN	GPIO_PinSource6
+#define ECHO_TIM_AF	GPIO_AF_TIM4
+#define ECHO_TIM_CHANNEL	TIM_Channel_1	
+#define ECHO_TIM_IRQ	TIM4_IRQn;
 
-#define TRIG_GPIO_PORT	GPIOC
-#define TRIG_GPIO_PIN	GPIO_Pin_6
-#define TRIG_GPIO_CLK	RCC_AHB1Periph_GPIOC
-#define TRIG_TIM	TIM3
-#define TRIG_TIM_CLK	RCC_APB1Periph_TIM3
-#define TRIG_TIM_PIN	GPIO_PinSource6
-#define TRIG_TIM_AF	GPIO_AF_TIM3
-#define TRIG_PWM_CHANNEL	TIM_OCMode_PWM2
+#define TRIG_GPIO_PORT	GPIOA
+#define TRIG_GPIO_PIN	GPIO_Pin_0
+#define TRIG_GPIO_CLK	RCC_AHB1Periph_GPIOA
+#define TRIG_TIM	TIM5
+#define TRIG_TIM_CLK	RCC_APB1Periph_TIM5
+#define TRIG_TIM_PIN	GPIO_PinSource0
+#define TRIG_TIM_AF	GPIO_AF_TIM5
+#define TRIG_PWM_CHANNEL	TIM_OCMode_PWM1
 #define TRIG_TIM_CHANNEL	TIM_Channel_1
+#define TRIG_TIM_IRQ	TIM5_IRQn;
 
 
 /* Private variables ---------------------------------------------------------*/
@@ -39,6 +40,8 @@
 void ultrasonic_init(void);
 void ultrasonic_trig(void);
 void ultrasonic_listen(void);
+uint32_t get_distance_cm(void);
+uint32_t get_distance_in(void);
 
 /* Private functions ---------------------------------------------------------*/
 static void _input_capture_config(void); // setup for echo pin
